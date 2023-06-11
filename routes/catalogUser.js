a43d56a73a2364b1ae5f32eae3d227d283b1e657
José Antonio <toni97jose@gmail.com>
2023-06-11 13:41:15 -0600

    Add welcome message

--- routes/catalogUser.js
+++ routes/catalogUser.js
@@ -5,6 +5,9 @@ const authMiddleware = require("../middlewares/authVerify");
 const upload = require('../middlewares/multer');
 
 //Crud routes
+router.get('/',function(req, res){
+    res.send({message:'Welcome'});
+})
 router.get("/usersCatalog", authMiddleware, UserCatalogController.getAllUsersOnCatalogs);
 router.post("/usersCatalog", authMiddleware, UserCatalogController.postOneUserOnCatalog);
 router.patch("/usersCatalog/:id", authMiddleware, UserCatalogController.patchOneUserOnCatalog);

4eb3e4b7e5446ef7b828f9458e604c3aef8a4ea5
José Antonio <toni97jose@gmail.com>
2023-06-11 15:03:51 -0600

    Change: Base path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Api routes
 app.use(cors());
-app.use('/v1/', indexRouter);
+app.use('/api/v1/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
